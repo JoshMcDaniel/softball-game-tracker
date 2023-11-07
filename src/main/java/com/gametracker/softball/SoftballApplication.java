3d3c6e594951f495115aa0c22b977798a2542d66
JoshMcDaniel <jrmcdaniel08@yahoo.com>
2023-11-06 23:54:14 -0500

    Remove dead code

--- src/main/java/com/gametracker/softball/SoftballApplication.java
+++ src/main/java/com/gametracker/softball/SoftballApplication.java
@@ -6,24 +6,17 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Objects;
 
 @SpringBootApplication
 public class SoftballApplication {
 
 	public static void main(String[] args) throws IOException {
 
+		// // Load serviceAccountKey.json as a file input stream
 		String serviceAccountKeyPath = System.getenv("SERVICE_ACCOUNT_KEY");
 		FileInputStream serviceAccount = new FileInputStream(serviceAccountKeyPath);
-
-		if (serviceAccount == null) {
-			// Handle the case when the resource is not found
-			throw new RuntimeException("serviceAccountKey.json not found in the classpath");
-		}
 
 		FirebaseOptions options = new FirebaseOptions.Builder()
 				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
